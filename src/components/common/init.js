acd00384c9ad2eeb63191cc12b4ec5b652d0040b
yaolong <470080502@qq.com>
2019-03-13 09:19:00 +0800

    change the init.js

--- src/components/common/init.js
+++ src/components/common/init.js
@@ -1,9 +1,25 @@
-const Init = function () {
-    let bodyWidth = document.body.clientWidth;
-    let bodyHeight = document.body.clientHeight;
+;(function (doc, win) {
+    var docEl = doc.document;
+    var resizeEvt = 'orientationchage' in win ? 'orientationchange' : 'resize';
+    var recalc = function () {
+        var clientWidth = docEl.clientWidth;
+        if (clientWidth === undefined) return
+        if (clientWidth > 1374) {
+            clientWidth = 1374;
+        }
+        if (clientWidth < 920) {
+            clientWidth = 920;
+        }
 
-    let htmlDom = document.getElementsByTagName("html")[0];
-    htmlDom.style.cssText = 'font-size: 16px;';
-}
+        docEl.style.fontSize = 16 * (clientWidth / 1374) + 'px';
+        if (window.orientation === 90) {
 
-export default Init
+        } else {
+            document.body.setAttribute('data-orient', 'landscape');
+        }
+
+        if (doc.addEventListener === undefined) return;
+        win.addEventListener(resizeEvt, recalc, false);
+        doc.addEventListener('DOMContentLoaded', recalc, false);
+    }
+})(document, window)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Init from './components/common/init';
-import Login from './components/Login/login';
+import './components/Login/login';
 import Header from './components/common/header';
 import Footer from './components/common/footer';
 require('normalize.css');
 require('../public/assets/css/global.less');
 
-Init();
 
 let headerContainer = document.getElementById('header');
 ReactDom.render(<Header />, headerContainer);
