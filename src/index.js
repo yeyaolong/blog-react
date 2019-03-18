e8745112a86fa42919f477c22eb525d2eabdb209
yeyl <470080502@qq.com>
2019-03-18 22:56:16 +0800

    add mockjs

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ import  '../public/assets/js/init';
 import Login from './components/Login/login';
 import Header from './components/common/header';
 import Footer from './components/common/footer';
+import MockMachine from './mock'
+
 require('normalize.css');
 require('../public/assets/css/global.less');
 
+
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+if (process.env.NODE_ENV == 'development') {
+    /* 仅在开发环境中，使用mock */
+    MockMachine.startMock();
+}
 
 let headerContainer = document.getElementById('header');
 ReactDom.render(<Header />, headerContainer);
